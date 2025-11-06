0c9b48b02b9d607c69d49f623540d886bb8a13d7
Ya-az <yazeedalharbe8@gmail.com>
2025-11-06 21:05:37 +0300

    Add React app with Vite and production build

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Triply | رحلاتك بتنظيم أنيق</h1>
+      <p>مرحباً بك في موقع Triply! الموقع يعمل بنجاح 🎉</p>
+    </div>
+  );
+}
+
+export default App;
